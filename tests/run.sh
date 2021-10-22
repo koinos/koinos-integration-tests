ba2cdebba0bb2bd467143e09a5b98794faf98bae
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-10-21 20:09:22 -0700

    Build test before starting node

--- tests/run.sh
+++ tests/run.sh
@@ -7,6 +7,7 @@ function run_tests() {
    success=0
    for test_dir in */ ; do
       pushd $test_dir
+      go build ./...
       docker-compose up -d
       success=$?||$success
       go test -v ./...
