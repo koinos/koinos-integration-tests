00af4e5ddeac56c21bff6a70254a02d4174ce4e6
Steve Gerbino <steve@gerbino.co>
2022-03-31 16:15:54 -0400

    Improvements to bash integration script

--- tests/run.sh
+++ tests/run.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
 function run_tests() {
-   local success=0
+   local code=0
 
    for test_dir in */ ; do
       pushd $test_dir
       go build ./...
+      if [ $? -ne 0 ];
+      then
+        echo "Failed to build integration test: ${test_dir}"
+        code=1
+        popd
+        continue
+      fi
+
       docker-compose up -d
-      if [ $? -ne 0 ] || [ $success -ne 0 ];
+      if [ $? -ne 0 ];
       then
-        success=1
+        echo "Failed to start cluster: ${test_dir}"
+        code=1
+        docker-compose logs
+        docker-compose down
+        popd
+        continue
       fi
+
       go test -v ./...
-      if [ $? -ne 0 ] || [ $success -ne 0 ];
+      if [ $? -ne 0 ];
       then
-        success=1
+        echo "Failed during integration test: ${test_dir}"
+        docker-compose logs
+        code=1
       fi
-      docker-compose logs
+
       docker-compose down
       popd
    done
@@ -26,4 +42,3 @@ function run_tests() {
 
 run_tests
 exit $?
-
